65c6b033d7678b86380e6f9274a52fbfdfdbb13c
LouiMinister <LouiMinister@gmail.com>
2020-09-19 16:27:13 +0900

    solve problem

--- javascript/programmers/level1/harshad.js
+++ javascript/programmers/level1/harshad.js
@@ -23,3 +23,12 @@ arr	return
 입출력 예 #4
 13의 모든 자릿수의 합은 4입니다. 13은 4로 나누어 떨어지지 않으므로 13은 하샤드 수가 아닙니다.
 */
+
+const solution = x => {
+    const sum = new String(x)
+    .split("")
+    .reduce( (sum,char) => sum += Number(char) ,0);
+    return (x % sum == 0) ? true : false;
+}
+
+console.log(solution(12));
