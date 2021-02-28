ee51f62dc44592d7699f7de94633bc11982b673a
LouiMinister <louiminister@gmail.com>
2021-02-28 20:08:43 +0900

    solve: 1000 A+B

---
+++ typescript/backjoon/0000.ts
@@ -0,0 +1,7 @@
+import fs = require('fs');
+//let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+let input = ''.toString().split(' ');
+console.log(input.toString());
+const a = parseInt(input[0]);
+const b = parseInt(input[1]);
+console.log(a+b);
