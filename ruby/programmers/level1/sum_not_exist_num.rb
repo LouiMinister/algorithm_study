dbb32a60477a1785a56d918e0ee45331a74cfa0a
LouiMinister <louiminister@gmail.com>
2023-01-04 15:44:22 +0900

    solve:ruby:프로그래머스:level1:음양더하기

--- ruby/programmers/level1/sum_not_exist_num.rb
+++ ruby/programmers/level1/sum_not_exist_num.rb
@@ -1,28 +1,29 @@
 ##
-# 없는 숫자 더하기
+# 음양 더하기
 # 문제 설명
-# 0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다. numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
+# 어떤 정수들이 있습니다. 이 정수들의 절댓값을 차례대로 담은 정수 배열 absolutes와 이 정수들의 부호를 차례대로 담은 불리언 배열 signs가 매개변수로 주어집니다. 실제 정수들의 합을 구하여 return 하도록 solution 함수를 완성해주세요.
 #
 # 제한사항
-# 1 ≤ numbers의 길이 ≤ 9
-# 0 ≤ numbers의 모든 원소 ≤ 9
-# numbers의 모든 원소는 서로 다릅니다.
+# absolutes의 길이는 1 이상 1,000 이하입니다.
+# absolutes의 모든 수는 각각 1 이상 1,000 이하입니다.
+# signs의 길이는 absolutes의 길이와 같습니다.
+# signs[i] 가 참이면 absolutes[i] 의 실제 정수가 양수임을, 그렇지 않으면 음수임을 의미합니다.
 # 입출력 예
-# numbers	result
-# [1,2,3,4,6,7,8,0]	14
-# [5,8,4,0,6,7,9]	6
+# absolutes	signs	result
+# [4,7,12]	[true,false,true]	9
+# [1,2,3]	[false,false,true]	0
 # 입출력 예 설명
 # 입출력 예 #1
 #
-# 5, 9가 numbers에 없으므로, 5 + 9 = 14를 return 해야 합니다.
+# signs가 [true,false,true] 이므로, 실제 수들의 값은 각각 4, -7, 12입니다.
+# 따라서 세 수의 합인 9를 return 해야 합니다.
 # 입출력 예 #2
 #
-# 1, 2, 3이 numbers에 없으므로, 1 + 2 + 3 = 6을 return 해야 합니다.
+# signs가 [false,false,true] 이므로, 실제 수들의 값은 각각 -1, -2, 3입니다.
+# 따라서 세 수의 합인 0을 return 해야 합니다.
 
-def solution(numbers)
-  check = Array.new(10) { |i| false }
-  numbers.each { |num| check[num] = true }
-  check.each_with_index.reduce(0) do |res, (bool, idx)|
-    !bool ? res + idx : res
+def solution(absolutes = [], signs)
+  return absolutes.each_with_index.reduce(0) do |res, (abs, idx)|
+    res + (signs[idx] ? abs : -abs)
   end
 end
