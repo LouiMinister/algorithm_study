8b2ffab1f52a326c75f4a80b60881e5501e86422
LouiMinister <louiminister@gmail.com>
2023-05-03 11:33:42 +0900

    setup java

--- java/backjoon/1001:A-B.java
+++ java/backjoon/1001:A-B.java
@@ -1,8 +1,10 @@
+package backjoon;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main{
+class Main{
 
   public static void main(String[] args) throws Exception{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
